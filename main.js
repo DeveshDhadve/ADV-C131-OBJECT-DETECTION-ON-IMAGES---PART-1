da49de6fbf68146ec7d8f23c031f76ac7a5c40d8
DeveshDhadve <91677052+DeveshDhadve@users.noreply.github.com>
2022-12-09 20:34:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function tvandac()
+{
+    window.location = "tvandac.html";
+}
+
+function desk()
+{
+    window.location = "desk.html";
+}
+
+function bottle()
+{
+    window.location = "bottle.html";
+}
+
+function fruit()
+{
+    window.location = "fruit.html";
+}
